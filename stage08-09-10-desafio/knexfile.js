3cc02b115e77af84126a71d2ce47443b098ee1cc
vitoria <vitorianobre@alu.ufc.br>
2023-04-06 10:29:35 -0300

    stage08 - desafio

---
+++ stage08-09-10-desafio/knexfile.js
@@ -0,0 +1,24 @@
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+const path = require("path");
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
+  },
+
+};
